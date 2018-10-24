e78295cbe5a3116c05f98472bf50107ab0aed7ea
AbraKadabraAlakazam <43590629+AbraKadabraAlakazam@users.noreply.github.com>
2018-10-23 19:22:03 -0500

    Add files via upload
    Newest Version - not done yet.

--- Pirate.py
+++ Pirate.py
@@ -1,4 +1,5 @@
 import json
+from tkinter import *
 class Pirate:
     name  = ""
     ship = ""
@@ -20,6 +21,7 @@ class Pirate:
 class FileManager:
     path = "pirateDB.json"
 
+
     def writeToFile(self, idNum, obj):
         try:
             f = open(self.path, "r")
@@ -31,3 +33,37 @@ class FileManager:
         f = open(self.path, "w")
         json.dump(d, f)
         f.close()
+
+root = Tk()
+
+# Configure the window
+root.title("Pirate Database")
+root.config(bg="LightBlue")
+
+# The Objects
+title = Label(root, text = "Pirate Database", font = "Arial 25 bold", bg ="LightBlue")
+name = Label(root, text = "Name", font = "Arial 20", bg ="LightGreen")
+ship = Label(root, text = "Ship", font = "Arial 20", bg ="LightGreen")
+Fictional = Label(root, text = "Fictional", font = "Arial 20", bg ="LightGreen")
+nText = Entry(root, font = "Arial 15", bg ="LightGreen")
+nShip = Entry(root, font = "Arial 15", bg ="LightGreen")
+save = Button(root, font = "Arial 20", text = "Save", bg ="LightGreen")
+
+# The Grid
+title.grid(row = 0, column = 0, columnspan = 3)
+name.grid(row = 1, column = 0)
+ship.grid(row = 2, column = 0)
+Fictional.grid(row = 3, column  = 0)
+nText.grid(row = 1, column = 1)
+nShip.grid(row = 2, column = 1)
+save.grid(row = 4, column = 0, columnspan = 3)
+
+# The Dropdown menus 
+optionString = StringVar(root)
+optionString.set("True")
+dropdown = OptionMenu(root, optionString, "True", "False")
+dropdown.config(font = "Arial 12", width = "10", bg = "LightGreen")
+dropdown.nametowidget(dropdown.menuname).config(font  = "Arial 12", bg = "LightGreen")
+dropdown.grid(row = 3, column = 1)
+
+root.mainloop()        
