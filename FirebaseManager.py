68416ef10e02df1e16a85e770785ff5d32ec6507
AbraKadabraAlakazam <43590629+AbraKadabraAlakazam@users.noreply.github.com>
2018-11-27 19:11:01 -0600

    Add files via upload
    Don't wanna lose my code, so uploading just in case.

---
+++ FirebaseManager.py
@@ -0,0 +1,11 @@
+from firebase import firebase as fb
+
+class FirebaseManager:
+    app = fb.FirebaseApplication("https://pirate-db-ab58b.firebaseio.com/", None)
+
+    def writeToFile(self, idNum, obj):
+        results = self.app.put("", idNum, obj)
+
+    def getAll(self):
+        d = self.app.get("", None)
+        return d
--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 #Imports
 from tkinter import *
+import FirebaseManager
 
 #Main window
 window1 = Tk()
@@ -9,8 +10,36 @@ window1.config(bg="salmon")
 def listDelete():
     listbox.delete(ANCHOR)
 def ext():
+    
     window1.destroy()
+    
+def doFilter():
+    filt = entry.get()
+    listbox.delete(0, "end")
+    for pirate in d:
+        if (filt.lower() in d[pirate]["name"].lower() or
+        filt.lower() in d[pirate]["ship"].lower()):
+            listbox.insert(END, d[pirate]["name"])
 
+def searchUpdate(e):
+    doFilter()
+    
+def display(pirateId):
+    pirateName.config(text =d[pirateId]["name"])
+    shipLabel.config(text = "Ship: " + d[pirateId]["ship"])
+    if d[pirateId]["fic"] == "True":
+        ficLabel.config(text = "This pirate is fictional!")
+    else:
+        ficLabel.config(text = "This pirate was/is real!")
+
+def onselect(e):
+    w = e.widget
+    index  = int(w.curselection()[0])
+    piratename = w.get(index)
+    for pirate in d:
+        if piratename.lower() == d[pirate]["name"].lower():
+            display(pirate)
+    
 #The Frames
 frame1 = Frame(window1)
 frame1.config(bg="salmon")
@@ -31,6 +60,7 @@ label1.pack()
 #Frame2 items
 label2 = Label(frame2, text = "Search", font = cm, bg = "salmon", fg="white")
 entry = Entry(frame2, font = cm, bg = "olive", fg="white")
+entry.bind("<KeyRelease>", searchUpdate)
 label2.grid(row = 0, column = 0)
 entry.grid(row = 0, column = 1)
 
@@ -41,14 +71,14 @@ rightImg = rightImg.subsample(2)
 leftImg = leftImg.subsample(2)
 leftBtn = Button(frame3, image = leftImg, bg = "olive")
 rightBtn = Button(frame3, image = rightImg, bg = "olive")
-pirateName = Label(frame3, text = "Pirate", bg  = "salmon", font = ("Comic Sans MS", 30), fg = "white")
+pirateName = Label(frame3, text = "Example Pirate", bg  = "salmon", font = ("Comic Sans MS", 30), fg = "white")
 leftBtn.grid(row = 1, column = 0, rowspan = 4)
 rightBtn.grid(row = 1, column = 2, rowspan = 4)
 pirateName.grid(row = 0, column = 1)
 fillerImg = PhotoImage(file = "knoi.gif")
 pirateLabel = Label(frame3, image = fillerImg, bg = "salmon")
 pirateLabel.grid(row = 1, column = 1)
-shipLabel = Label(frame3, text = "Ship: The Rugberry", bg = "salmon", fg = "white", font = cm)
+shipLabel = Label(frame3, text = "Ship: The Example Ship", bg = "salmon", fg = "white", font = cm)
 ficLabel = Label(frame3, text = "Fictional: True", bg = "salmon", fg = "white", font = cm)
 shipLabel.grid(row = 2, column = 1)
 ficLabel.grid(row = 3, column = 1)
@@ -57,14 +87,17 @@ ficLabel.grid(row = 3, column = 1)
 label4 = Label(frame4, text = "View Pirate", font = cm, bg = "salmon", fg = "white")
 label4.pack() 
 listbox = Listbox(frame4, font = cm, bg = "Olive", fg="white")
+listbox.bind("<<ListboxSelect>>", onselect)
 listbox.pack()
-listDict = {"Jerry":"1", "Gerry":"2", "Jeri":"3", "Gerri":"4"}
+fm = FirebaseManager.FirebaseManager()
+d = fm.getAll()
+for item in d:
+    pirate = d[item]
+    listbox.insert(END, pirate["name"])
 deleteButton = Button(frame4, text="Delete", font = cm, command=listDelete, bg = "Olive", fg="white")
 deleteButton.pack()
 extButton = Button(frame4, text="Exit", font = cm, command=ext, bg = "Olive", fg="white")
 extButton.pack()
-for item in listDict:
-    listbox.insert(END, item)
     
 #The Frames
 frame1.grid(row = 0, column = 0)
